08ef656de0f589ab95cc5c9440c7f4dd1edd87f5
Gabriel Csapo <gabecsapo@gmail.com>
2020-04-16 22:49:40 -0700

    [feature] be able to search plugins list by fuzzy search

--- app/components/node-list.ts
+++ app/components/node-list.ts
@@ -1,4 +1,5 @@
 import Component from "@glimmer/component";
+import { action } from '@ember/object';
 
 // TODO: you can generate this from the GraphQL queries!
 interface Node {
@@ -11,7 +12,19 @@ interface Args {
   nodes?: Node[];
 }
 
+interface DOMEvent {
+  target: {
+    value: string
+  }
+}
+
 export default class NodeList extends Component<Args> {
+  @action
+  searchNode(e: DOMEvent) {
+    window.location.search = `?searchTerm=${e.target.value}`;
+  }
+
+
   get nodes() {
     return (
       this.args.nodes?.sort(
